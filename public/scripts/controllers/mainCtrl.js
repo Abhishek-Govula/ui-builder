dd79f3c4ca79dc3ae7aa64f243d9f569cc293460
abhishek <abhishek520.govula@gmail.com>
2016-07-15 19:10:56 +0530

    updated the controller and style libs

---
+++ public/scripts/controllers/mainCtrl.js
@@ -0,0 +1,5 @@
+var rootApp = angular.module('ui-creator', ['ui.router']);
+
+rootApp.controller('mainCtrl', ['$scope', function($scope){
+    $('#main-div').text("Jquery, Angular , UI Router, Bootstrap Libraries loaded")
+}]);
