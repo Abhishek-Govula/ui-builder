12c68d7920cdbc96c038d32f2749886362b94003
abhishek <abhishek520.govula@gmail.com>
2016-07-17 17:11:29 +0530

    chnaged the properties as a module

--- public/components/properties-module/propertiesCtrl.js
+++ public/components/properties-module/propertiesCtrl.js
@@ -7,8 +7,10 @@ propertiesModule.controller('propertiesCtrl', ['$scope', function ($scope) {
         {id: '2', name: 'Bar'},
         {id: '3', name: 'Pie'}
         ],
-        selectedOption: {id: '3', name: 'Pie'} //This sets the default value of the select in the ui
+        selectedOption: null //Will be using this later for getting the type of graph selected`
     };
+    $scope.data.selectedOption = $scope.data.availableGraphs[0]; //This sets the default value of the select in the ui
+
     $scope.graphOptionChanged = function(elem) {
         console.log("Graph select box has been changed");
     }
